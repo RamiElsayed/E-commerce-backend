d0db2d91bf1cd84857219882328d72af64c36092
Rami Elsayed <ramielsayed123@outlook.com>
2022-08-03 22:46:25 +0100

    established associations and product model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,12 +4,19 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  await Category.findAll({
-    include: {
-      model: Product,
-      attribute: ['product_name']
-    }
-  })
+  try {
+    const categoryData = await Category.findAll({
+      include: {
+        model: Product,
+        attribute: ['product_name']
+      }
+    }).map((categoryData) => categoryData.get({ plain: true }));
+
+    return res.json(categoryData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 router.get('/:id', (req, res) => {
